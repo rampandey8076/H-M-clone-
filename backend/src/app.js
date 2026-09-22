5fc09a78d07afff04d45c15d28634f8b969dbae5
pandey rama <rampandey92500@gmail.com>
2026-09-22 22:27:09 +0530

    add all route and create post method

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,24 @@ const app = express()
 
 app.get("/",(req,res)=>{
     res.send("hello world");
+    
 })
+
+
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+
 app.get("/about",(req,res)=>{
     res.send("this is the about page")
 })
+
+app.all('*', (req, res) => {
+  res.status(404).send('404 - Page not found');
+});
+
+
 
 
 module.exports=app
